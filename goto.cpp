d41009deaeb45e9134a7948e38cb4b314cf1bea3
ionic <87571564+ionicbond12203@users.noreply.github.com>
2024-04-22 23:26:55 +0800

    Create goto.cpp

---
+++ goto.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct course{};
+
+int main() {
+	cout << "hello world" << endl;
+	cout << "hello github" << endl;
+	goto Flag;
+	cout << "programming " << endl;
+
+
+Flag:
+	cout << "hello ionic" << endl;
+	return 0;
+}
