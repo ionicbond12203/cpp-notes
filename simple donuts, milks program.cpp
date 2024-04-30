0fc97964b8b2b401f4b93e51af6ab7e4db624636
ionic <87571564+ionicbond12203@users.noreply.github.com>
2024-04-30 21:41:55 +0800

    Update and rename simple donuts, milks program to simple donuts, milks program.cpp

--- simple donuts, milks program.cpp
+++ simple donuts, milks program.cpp
@@ -10,7 +10,11 @@ int main() {
     cout << "Enter number of glasses of milk: ";
     cin >> milks;
 
-    double dpg = static_cast<double>(donuts) / milks;
+    /**This line (line 17) calculates the average number of donuts per glass of milk
+    by dividing the number of donuts by the number of glasses of milk.
+    The result is stored in the variable dpg as a double.**/
+    
+    double dpg = static_cast<double>(donuts) / milks; 
     
     cout << donuts << " donuts.\n"
          << milks << " glasses of milk.\n"
