162a4112ca1b82a5bb5a26edba75304ff0bf3a80
ionic <87571564+ionicbond12203@users.noreply.github.com>
2024-05-07 23:56:28 +0800

    Create testing.cpp

---
+++ testing.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void newLine();
+
+void getInt(int& number);
+
+int main(){
+    int n;
+    getInt();
+    cout<<"final value read in = "<<n<<endl;
+        <<"end of demostration.\n";
+        return 0;
+}
