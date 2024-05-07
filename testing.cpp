423e2691a05f31a8b665e2bf43cddeedfaf63d1a
ionic <87571564+ionicbond12203@users.noreply.github.com>
2024-05-08 00:01:14 +0800

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -8,7 +8,28 @@ void getInt(int& number);
 int main(){
     int n;
     getInt();
-    cout<<"final value read in = "<<n<<endl;
+    cout<<"final value read in = "<<n<<endl
         <<"end of demostration.\n";
         return 0;
+}
+
+void newLine(){
+    char symbol;
+    do{
+        cin.get(symbol);
+        
+    }while(symbol !='\n')
+}
+
+void getInt(int% number){
+    char ans;
+    do{
+        cout<<"enter input number: ";
+        cin>>number;
+        cout<<"you entered "<<number
+            <<". Is that correct? (yes/no): ";
+        cin>>ans;
+        newLine();
+        
+    }while ((ans!='y')&&(ans!='y'));
 }
